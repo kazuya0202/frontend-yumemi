0782afd64a5b5fe2f87bdae8734c45b5407df517
ichiya <kazuya7187@gmail.com>
2024-05-30 02:41:07 +0900

    都道府県の選択項目数を表示

--- src/features/Prefectures.tsx
+++ src/features/Prefectures.tsx
@@ -2,6 +2,7 @@ import styles from "./Prefectures.module.scss";
 
 import Badge from "@/components/Badge";
 import Checkbox from "@/components/Checkbox";
+import { useSelectedPrefCodes } from "@/hooks/usePopulation";
 import { ResasPrefecture } from "@/models/APIResponseType";
 
 type Props = {
@@ -10,9 +11,19 @@ type Props = {
 }
 
 export default function Prefectures({ prefs, onChange }: Props) {
+  const { selectedPrefCodes } = useSelectedPrefCodes();
+
   return <>
     <section>
-      <Badge>都道府県</Badge>
+      <div className={styles.title}>
+        <Badge>都道府県</Badge>
+        {selectedPrefCodes.size > 0 &&
+          <p>
+            {selectedPrefCodes.size}
+            <span className={styles.label}>項目選択中</span>
+          </p>
+        }
+      </div>
       <div className={styles.prefectures}>
         {prefs && prefs.map((pref) => {
           return <Checkbox key={pref.prefCode} value={pref.prefCode} onChange={onChange}>
