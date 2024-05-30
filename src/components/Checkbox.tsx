e245dfe360f1abf403221dd8ff76d10b733d54ad
ichiya <kazuya7187@gmail.com>
2024-05-31 00:29:22 +0900

    連続で都道府県を選択した際に挙動が不安定になる問題の解決 #20

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,5 +1,8 @@
+import { useRef } from "react";
+
 import styles from "./Checkbox.module.scss";
 
+import { useSelectedPrefCodes } from "@/hooks/usePopulation";
 
 type Props = {
   value: number;
@@ -8,10 +11,23 @@ type Props = {
 }
 
 export default function Checkbox({ value, onChange, children }: Props) {
+  const { selectedPrefCodes } = useSelectedPrefCodes();
+  const timerRef = useRef<number | null>(null);
+
+  // 連続して選択された場合に対応するため 100ms タイムアウトする
+  const debounce = (event: React.ChangeEvent<HTMLInputElement>, callbackFn: (evnet: React.ChangeEvent<HTMLInputElement>) => void) => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+    timerRef.current = setTimeout(() => {
+      callbackFn(event);
+    }, 100);
+  };
+
   return (
     <div>
       <label className={styles.checkbox} >
-        <input type="checkbox" onChange={onChange} value={value} />
+        <input type="checkbox" onChange={(e) => debounce(e, onChange)} value={value} checked={selectedPrefCodes.has(value)} />
         {children}
       </label>
     </div>
--- src/hooks/usePopulation.ts
+++ src/hooks/usePopulation.ts
@@ -1,11 +1,10 @@
 import { atom, useAtom } from "jotai";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 import { usePrefectures } from "@/hooks/usePrefectures";
 import { ResasAPIResponse, ResasPopulation } from "@/models/APIResponseType";
 import { PopulationElement } from "@/models/ChartElements";
 import { client } from "@/utils/resasClient";
-
 
 /**
  * 取得済みの都道府県別のデータ
@@ -30,40 +29,48 @@ export const useFetchPopulation = () => {
   const { population, setPopulation } = useFetchedPopulation();
   const { prefs } = usePrefectures();
 
-  useEffect(() => {
-    const fetchPopulationData = async (prefCode: number) => {
-      // console.log(`fetch ${prefCode}`);
-      const res = await client
-        .get<ResasAPIResponse<ResasPopulation>>(
-          "/api/v1/population/composition/perYear",
-          {
-            params: { cityCode: "-", prefCode: prefCode },
-            headers: { "X-API-KEY": import.meta.env.VITE_RESAS_API_KEY }
-          }
-        );
+  const isFetchingAPI = useRef<boolean>(false);
 
-      const prefName = prefs?.find((d) => d.prefCode === prefCode)?.prefName;
-      const elm: PopulationElement = {
-        prefName: prefName!,
-        prefCode: prefCode,
-        data: [],
-      };
-      for (const item of res.data.result.data) {
-        elm.data.push({
-          label: item.label,
-          data: item.data.filter((d) => d.year <= res.data.result.boundaryYear),
-        });
-      }
-      setPopulation([...population, elm]);
-      setSelectedPrefCodes(new Set([...selectedPrefCodes, prefCode]));
+  const fetchPopulationData = async (prefCode: number) => {
+    const res = await client
+      .get<ResasAPIResponse<ResasPopulation>>(
+        "/api/v1/population/composition/perYear",
+        {
+          params: { cityCode: "-", prefCode: prefCode },
+          headers: { "X-API-KEY": import.meta.env.VITE_RESAS_API_KEY }
+        }
+      );
+
+    const prefName = prefs?.find((d) => d.prefCode === prefCode)?.prefName;
+    const elm: PopulationElement = {
+      prefName: prefName!,
+      prefCode: prefCode,
+      data: [],
     };
+    for (const item of res.data.result.data) {
+      elm.data.push({
+        label: item.label,
+        data: item.data.filter((d) => d.year <= res.data.result.boundaryYear),
+      });
+    }
+    setPopulation([...population, elm]);
+    setSelectedPrefCodes(new Set([...selectedPrefCodes, prefCode]));
+    isFetchingAPI.current = false;
+  };
+
+  useEffect(() => {
+    // 人口構成データを取得中なら return（同時に1つだけ実行する）
+    if (isFetchingAPI.current) {
+      return;
+    }
 
     for (const prefCode of selectedPrefCodes) {
+      // すでに取得している場合はスキップ
       if (population.find((p) => p.prefCode === prefCode)) {
-        // console.log(`skip ${prefCode}`);
         continue;
       }
 
+      isFetchingAPI.current = true;
       fetchPopulationData(prefCode);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
